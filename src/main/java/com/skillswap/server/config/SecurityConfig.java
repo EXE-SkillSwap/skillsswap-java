0d593e6c87dd4ea9104bc06ec07b6f3e57751b87
anhnhv2503 <nguyenhavietanh2503@gmail.com>
2025-06-24 23:35:35 +0700

    fix cors 2

--- src/main/java/com/skillswap/server/config/SecurityConfig.java
+++ src/main/java/com/skillswap/server/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
     public CorsFilter corsFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
