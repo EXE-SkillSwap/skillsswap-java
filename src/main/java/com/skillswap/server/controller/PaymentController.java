c7651214ff5ddbc693df3dbf42176aad1222ab6d
anhnhv2503 <nguyenhavietanh2503@gmail.com>
2025-07-22 23:53:49 +0700

    fix cors payment controller

--- src/main/java/com/skillswap/server/controller/PaymentController.java
+++ src/main/java/com/skillswap/server/controller/PaymentController.java
@@ -4,14 +4,12 @@ import com.skillswap.server.dto.request.WebhookUrlRequest;
 import com.skillswap.server.services.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payment")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class PaymentController {
 
     private final PaymentService paymentService;
@@ -20,7 +18,7 @@ public class PaymentController {
     public ResponseEntity<?> confirmWebhook(@RequestBody WebhookUrlRequest request){
         try {
             var response = paymentService.confirmWebhook(request.getWebhookUrl());
-            return ResponseEntity.ok("Webhook confirmed successfully");
+            return ResponseEntity.ok("Webhook confirmed successfully" + response);
         } catch (Exception e) {
             throw new RuntimeException("Failed to confirm webhook");
         }
